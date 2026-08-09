d610eed126bd0e009b65185c5bfe6f2ac5f6ba33
Luiz Miguel Bastos <luizmiguelbastos90@gmail.com>
2026-08-09 14:21:36 -0300

    Criação da classe Usuario pra ser a Entidade

---
+++ src/main/java/usuarios/Usuario.java
@@ -0,0 +1,27 @@
+package Usuarios;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import org.hibernate.annotations.AnyDiscriminatorImplicitValues;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity
+public class Usuario {
+
+    private String nome;
+    private String username;
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String senha;
+
+    @CreationTimestamp
+    private LocalDateTime criadoEm;
+}
---
+++ src/main/java/usuarios/UsuarioController.java
@@ -0,0 +1,7 @@
+package Usuarios;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UsuarioController {
+}
