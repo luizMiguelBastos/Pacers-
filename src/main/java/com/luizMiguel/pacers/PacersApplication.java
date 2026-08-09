69362d09368b8b17aecc982a894db15e818e49ad
Luiz Miguel Bastos <luizmiguelbastos90@gmail.com>
2026-08-09 13:40:51 -0300

    primeiro commit

---
+++ src/main/java/com/luizMiguel/pacers/PacersApplication.java
@@ -0,0 +1,13 @@
+package com.luizMiguel.pacers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PacersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luizMiguel/pacers/PacersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizMiguel.pacers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PacersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
